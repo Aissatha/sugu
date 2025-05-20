30d6cf64a78adc071483ad4b4a68ddbf862a7e01
Aissatha <aissathafomba@gmail.com>
2025-05-20 17:05:38 +0100

    Fonctionnalitées ADMIN

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::paginate(10);
+        $users = User::with('roles')->orderBy('created_at', 'desc')->paginate(10);
         return view('admin.users.index', compact('users'));
     }
 
